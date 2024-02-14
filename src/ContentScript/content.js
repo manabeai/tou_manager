1634be3a90189be1a339bd44593ba97b5aa820b6
manabeai <matsu.devtool@gmail.com>
2024-02-15 08:57:02 +0900

    window.addEventListener内の処理を関数化 observerを追加して発火のタイミングを合うように

--- src/ContentScript/content.js
+++ src/ContentScript/content.js
@@ -12,7 +12,7 @@ function createSubjectDict() {
     subjectDict[subject_ID] = subject_name;
   });
   
-  console.log('subjectDict作成');
+  console.log('subjectDictを作成しました');
   return subjectDict;
 }
 
@@ -25,62 +25,65 @@ function extractNumberFromString(str) {
   return null;
 }
 
+const getSubjectAndId = function() {
+  const subjectDict = createSubjectDict();
+  console.log(subjectDict);
+  // 講義情報の初期化はbackgroundにわたす
+  chrome.runtime.sendMessage({message: 'open_progress', subjectDict: subjectDict}, 
+    function(response) {
+      if (response && response.message === 'initialized_score') {
+        alert('得点状況を初期化しました')
+      }
+    }
+  );
+}
+
+const getResult = function() {
+  const currentURL = window.location.href
+  const subject_ID = resultPage.exec(currentURL)[1];
+  
+  // 回を取得
+  const titleElement = document.querySelector('.tou-title');
+  const titleText = titleElement.textContent;
+  const currentSession = extractNumberFromString(titleText);
+  console.log('第' + currentSession + '回');
+  
+  // 得点を取得
+  const correctPointElement = document.querySelector('.correct-num span');
+  const correctPointText = correctPointElement.textContent;
+  const correctPoint = parseInt(correctPointText, 10);
+  console.log(correctPoint + '点'); 
+  
+  // 最大得点の更新はバックグラウンドに渡す
+  chrome.runtime.sendMessage({
+    message: 'finished_quiz',
+    currentSession: currentSession,
+    correctPoint: correctPoint,
+    subject_ID: subject_ID},
+    
+    function(response) {
+    if (response && response.message === 'updated_score') {
+      alert('得点状況を更新しました');
+    }
+  });
+}
+
+// 適切なタイミングで情報取得のイベントを発火させるためにobserverを使用
+const progressObserver = new MutationObserver(() => (getSubjectAndId(), progressObserver.disconnect()));
+const resultObserver = new MutationObserver(() => (getResult(), resultObserver.disconnect()));
+const config = { attributes: true, childList: true, subtree: true };
+
 const resultPage = /https:\/\/room\.internet\.ac\.jp\/#\/courses\/(\d+)\/sections\/\d+\/lectures\/\d+\/quiz\/result/;
 
-// progress画面を開いた際に科目名とIDを取得する
-window.onpopstate = function(event) {
+window.addEventListener("hashchange", function() {
+  // progress画面を開いた際に科目名とIDを取得しkey value方式で初期化する
   if (window.location.href === 'https://room.internet.ac.jp/#/progress') {
+    const progressTargetNode = document.querySelector(".content > .spacer > .page");
+    progressObserver.observe(progressTargetNode, config);
 
-      // loadが完了しても完全に表示されないので5秒ほど待つ
-      setTimeout(function() {
-        const subjectDict = createSubjectDict();
-        console.log(subjectDict);
-
-        // 講義情報の初期化はbackgroundにわたす
-        chrome.runtime.sendMessage({message: 'open_progress', subjectDict: subjectDict}, 
-          function(response) {
-            if (response && response.message === 'initialized_score') {
-              alert('得点状況を初期化しました')
-            }
-          }
-        );
-      }, 7000);
-    
-    // 結果画面が出た時に得点、回数、科目IDを取得
-    } else if (resultPage.test(window.location.href)){
-
-      setTimeout(function() {
-
-      console.log('得点を更新します');
-      const currentURL = window.location.href
-      const subject_ID = resultPage.exec(currentURL)[1];
-      
-      // 回を取得
-      const titleElement = document.querySelector('.tou-title');
-      const titleText = titleElement.textContent;
-      const currentSession = extractNumberFromString(titleText);
-      console.log('第' + currentSession + '回');
-      
-      // 得点を取得
-      const correctPointElement = document.querySelector('.correct-num span');
-      const correctPointText = correctPointElement.textContent;
-      const correctPoint = parseInt(correctPointText, 10);
-      console.log(correctPoint + '点'); 
-      
-      // 最大得点の更新はバックグラウンドに渡す
-      chrome.runtime.sendMessage({
-        message: 'finished_quiz',
-        currentSession: currentSession,
-        correctPoint: correctPoint,
-        subject_ID: subject_ID
-      },
-      
-      function(response) {
-        if (response) {
-          alert('得点状況を更新しました');
-        }
-      });
-    }, 5000);
+  // 結果画面が出た時に得点、回数、科目IDを取得、ストレージにある得点と比較して高ければ更新
+  } else if (resultPage.test(window.location.href)){
+    const resultTargetNode = document.querySelector('.content > .spacer');
+    resultObserver.observe(resultTargetNode, config);
   }
-};
-
+});
